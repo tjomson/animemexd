932171285d6dead6f4cc28d7d4055d8f05c578d5
Thomas Hoffmann Kilbak <thhk@itu.dk>
2020-11-12 23:23:04 +0100

    Add choosing user and type

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 //npm install jikanjs --save
-
 function test() {
+    let types = document.getElementById("types");
+    let username = document.getElementById("username");
     //console.log(jikanjs.loadUser("tjoms", "completed"));
-    let response = httpGet("https://api.jikan.moe/v3/user/tjoms/animelist/completed");
+    let response = httpGet("https://api.jikan.moe/v3/user/"+ username.value +"/animelist/completed");
     let asJson = JSON.parse(response);
-    console.log(asJson['anime'][0]);
+    let animesOfType = [];
+    for (let anime of asJson['anime']) {
+        if (anime['type'] === types.value) {
+            animesOfType.push(anime);
+        }
+    }
+
+    console.log(asJson);
+    console.log(animesOfType);
+
+
 }
 
-function httpGet(theUrl)
-{
+function httpGet(theUrl) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     xmlHttp.send( null );
